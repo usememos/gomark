cef1ce9a3856ad545ee00206d930d021fd4169e3
Steven <stevenlgtm@gmail.com>
2024-01-30 19:44:14 +0800

    chore: tweak comments

--- gomark.go
+++ gomark.go
@@ -7,11 +7,13 @@ import (
 	"github.com/usememos/gomark/restore"
 )
 
+// Parse parses the given markdown string and returns a list of nodes.
 func Parse(markdown string) (nodes []ast.Node, err error) {
 	tokens := tokenizer.Tokenize(markdown)
 	return parser.Parse(tokens)
 }
 
+// Restore restores the given nodes to a markdown string.
 func Restore(nodes []ast.Node) string {
 	return restore.Restore(nodes)
 }
